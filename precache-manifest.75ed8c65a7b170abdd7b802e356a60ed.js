df64e23173cce73a0327550c06f24a6c02e93bd7
prabhsembhi <prabhpreetsingh013@gmail.com>
2020-05-20 00:17:59 -0700

    Updates

---
+++ precache-manifest.75ed8c65a7b170abdd7b802e356a60ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5784af694626d40623ef66f127058dc3",
+    "url": "/Notes-Keeper/index.html"
+  },
+  {
+    "revision": "8d65ec444195668d0922",
+    "url": "/Notes-Keeper/static/js/2.688ceb67.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/Notes-Keeper/static/js/2.688ceb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edcedd83870e1d5dbabd",
+    "url": "/Notes-Keeper/static/js/main.5b78d882.chunk.js"
+  },
+  {
+    "revision": "0b571b31b2aea3c46b96",
+    "url": "/Notes-Keeper/static/js/runtime-main.0a12c16e.js"
+  }
+]);
